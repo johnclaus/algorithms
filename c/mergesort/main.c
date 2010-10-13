4bbfc8977f949a875d1ddbed143bfa396b0cdc0b
David Reynolds <david@alwaysmovefast.com>
2010-10-13 10:43:17 -0700

    made some comments about mergesort

--- c/mergesort/main.c
+++ c/mergesort/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /*
- * Exercise: Rewrite merge so it doesn't use sentinels. Instead, it should stop once either
+ * TODO: Rewrite merge so it doesn't use sentinels. Instead, it should stop once either
  * array L or R has had all its elements copied back into A and then copying the remainder
  * of the other array back into A.
  */
@@ -13,6 +13,13 @@ void merge(int *arr, int p, int q, int r) {
 
     n1 = q - p;
     n2 = r - q;
+
+    /*
+     * TODO: How can you design this algorithm so it either doesn't use extra space
+     * or it doesn't use malloc each time merge() is called? One way would be to
+     * use malloc to create a second array the same size as the first and pass it into
+     * merge().
+     */
 
     L = (int *)malloc((n1) * sizeof(int));
     R = (int *)malloc((n2) * sizeof(int));
@@ -25,7 +32,9 @@ void merge(int *arr, int p, int q, int r) {
         R[j] = arr[q+j];
     }
 
-    /* Should compute sentinel values from largest known element in arr */
+    /*
+     * Should compute sentinel values from largest known element in arr
+     */
     L[n1] = 999;
     R[n2] = 999;
 

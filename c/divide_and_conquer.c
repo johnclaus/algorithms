17aba95540f5cb5bd09c6ee359898c8b2b01ccd6
David Reynolds <david@alwaysmovefast.com>
2011-04-22 19:09:58 -0700

    renamed 'search' to 'find_largest'

--- c/divide_and_conquer.c
+++ c/divide_and_conquer.c
@@ -11,10 +11,12 @@
 
 #define N 1024
 
-int search(int A[], int i, int j) {
+int find_largest(int A[], int i, int j) {
     /*
-     * Finds the largest element in A[] and returns the index of that element.
-     * search(...) uses a divide-and-conquer approach to this problem.
+     * Finds the largest element in an unsorted array A[] and
+     * returns the index of that element.
+     *
+     * find_largest(...) uses a divide-and-conquer approach to this problem.
      * - The basic idea is to break the problem down until you're returning
      *   a single element's index. Once you do that, the algorithm can backtrack
      *   and compare the best elements of each branch (left and right).
@@ -25,11 +27,10 @@ int search(int A[], int i, int j) {
 
     if (i < j) {
         m = (i+j) / 2;
-        left = search(A, i, m);
-        right = search(A, m+1, j);
-        if (A[left] > A[right]) {
+        left = find_largest(A, i, m);
+        right = find_largest(A, m+1, j);
+        if (A[left] > A[right])
             return left;
-        }
         return right;
     }
     return i;
@@ -44,7 +45,8 @@ int main(int argc, char **argv) {
         A[i] = rand() % N+1;
     }
 
-    i = search(A, 0, N);
+    i = find_largest(A, 0, N);
     printf("A[%d] = %d\n", i, A[i]);
+
     return 0;
 }

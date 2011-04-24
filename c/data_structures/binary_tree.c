7ba57c1020617e0bf2cd8b87d7881806154a02ad
David Reynolds <david@alwaysmovefast.com>
2011-04-23 19:37:59 -0700

    renamed the functions and types

--- c/data_structures/binary_tree.c
+++ c/data_structures/binary_tree.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node_s {
+struct bst_node_s {
     int data;
-    struct node_s *parent;
-    struct node_s *left;
-    struct node_s *right;
+    struct bst_node_s *parent;
+    struct bst_node_s *left;
+    struct bst_node_s *right;
 };
 
 struct stack_s {
-    struct node_s *node;
+    struct bst_node_s *node;
     struct stack_s *next;
 };
 
-void push(struct stack_s **s, struct node_s *n) {
+void push(struct stack_s **s, struct bst_node_s *n) {
     struct stack_s *p;
     p = (struct stack_s *)malloc(sizeof(struct stack_s));
     p->node = n;
@@ -21,11 +21,11 @@ void push(struct stack_s **s, struct node_s *n) {
     *s = p;
 }
 
-struct node_s *pop(struct stack_s **s) {
+struct bst_node_s *pop(struct stack_s **s) {
     /* returns BST node pointed to by the top of the stack
      * and frees the stack element */
     struct stack_s *top;
-    struct node_s *n;
+    struct bst_node_s *n;
 
     top = *s;
     n = top->node;
@@ -36,28 +36,26 @@ struct node_s *pop(struct stack_s **s) {
     return n;
 }
 
-static struct node_s *new_node(int value) {
-    struct node_s *n;
-    n = (struct node_s *)malloc(sizeof(struct node_s));
+static struct bst_node_s *bst_new_node(int value) {
+    struct bst_node_s *n;
+    n = (struct bst_node_s *)malloc(sizeof(struct bst_node_s));
     n->data = value;
     n->left = NULL;
     n->right = NULL;
     return n;
 }
 
-/*
- *
- *          5
- *         / \
- *        3   7
- *       / \   \
- *      2   5   8
- *
- */
+void bst_inorder(struct bst_node_s *p) {
+    if (p) {
+        bst_inorder(p->left);
+        printf("  %d\n", p->data);
+        bst_inorder(p->right);
+    }
+}
 
 /* implement an iterative inorder traversal algorithm */
-void inorder2(struct node_s *p) {
-    struct node_s *q;
+void bst_inorder2(struct bst_node_s *p) {
+    struct bst_node_s *q;
     struct stack_s *stack;
 
     stack = NULL;
@@ -79,72 +77,64 @@ void inorder2(struct node_s *p) {
     }
 }
 
-void inorder(struct node_s *p) {
+void bst_preorder(struct bst_node_s *p) {
     if (p) {
-        inorder(p->left);
         printf("  %d\n", p->data);
-        inorder(p->right);
+        bst_preorder(p->left);
+        bst_preorder(p->right);
     }
 }
 
-void preorder(struct node_s *p) {
+void bst_postorder(struct bst_node_s *p) {
     if (p) {
-        printf("  %d\n", p->data);
-        preorder(p->left);
-        preorder(p->right);
-    }
-}
-
-void postorder(struct node_s *p) {
-    if (p) {
-        postorder(p->left);
-        postorder(p->right);
+        bst_postorder(p->left);
+        bst_postorder(p->right);
         printf("  %d\n", p->data);
     }
 }
 
-void destroy_bst(struct node_s *p) {
+void bst_destroy(struct bst_node_s *p) {
     if (p) {
-        destroy_bst(p->left);
-        destroy_bst(p->right);
+        bst_destroy(p->left);
+        bst_destroy(p->right);
         free(p);
     }
 }
 
 int main(int argc, char **argv) {
-    struct node_s *root;
+    struct bst_node_s *root;
 
-    root = new_node(5);
+    root = bst_new_node(5);
     root->parent = NULL;
 
-    root->left = new_node(3);
+    root->left = bst_new_node(3);
     root->left->parent = root;
 
-    root->right = new_node(7);
+    root->right = bst_new_node(7);
     root->right->parent = root;
 
-    root->left->left = new_node(2);
+    root->left->left = bst_new_node(2);
     root->left->left->parent = root->left;
 
-    root->right->right = new_node(8);
+    root->right->right = bst_new_node(8);
     root->right->right->parent = root->right;
 
-    root->left->right = new_node(5);
+    root->left->right = bst_new_node(5);
     root->left->right->parent = root->left;
 
     printf("inorder:\n");
-    inorder(root);
+    bst_inorder(root);
 
     printf("inorder2:\n");
-    inorder2(root);
+    bst_inorder2(root);
 
     printf("preorder:\n");
-    preorder(root);
+    bst_preorder(root);
 
     printf("postorder:\n");
-    postorder(root);
+    bst_postorder(root);
 
-    destroy_bst(root);
+    bst_destroy(root);
 
     return 0;
 }

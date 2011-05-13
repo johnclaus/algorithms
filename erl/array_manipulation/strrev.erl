9b64f402071fd9883314f4c8dd63e4706799a2d9
John Claus <johnclaus@gmail.com>
2011-05-12 19:21:22 -0600

    Added strrev:strrev_by_char

---
+++ erl/array_manipulation/strrev.erl
@@ -0,0 +1,14 @@
+%% -------------------------------------------------------------
+%%
+%% The strrev_by_char function is a simple call which takes a string
+%% as input and returns the reversed input by characters.
+%%
+%%	eg. 'abc' -> 'cba'
+%%		'abcdef ghij' -> 'jihg fedcba'
+%%
+%% -------------------------------------------------------------
+
+-module(strrev).
+-export([strrev_by_char/1]).
+
+strrev_by_char(String) -> lists:reverse(String).
